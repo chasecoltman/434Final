43ff82792c808cab65927dbada73e9f9cc924ad4
Chase Coltman <chase.coltman@gmail.com>
2017-06-08 12:26:52 -0700

    Add files via upload

---
+++ RanForestFinal.py
@@ -0,0 +1,188 @@
+# Reference: http://machinelearningmastery.com/implement-decision-tree-algorithm-scratch-python/
+
+#We used this reference as a sort of tutorial in order to implement a decision tree, so it might be wrong
+#We also changed the form of the data to make it a little easier to work through the tutorial with
+#What we changed was we moved the Y data to the far right instead of being on the far left
+#In this we used the CART Algorithm in order to print out the accuracy we discovered for the data
+
+from random import randrange
+from csv import reader
+
+# Load a CSV file
+def load_csv(filename):
+	file = open(filename, "rb")
+	lines = reader(file)
+	dataset = list(lines)
+	return dataset
+
+# Convert string column to float
+def str_column_to_float(dataset, column):
+	for row in dataset:
+		row[column] = float(row[column].strip())
+
+# Split a dataset into k folds
+def cross_validation_split(dataset, n_folds):
+	dataset_split = list()
+	dataset_copy = list(dataset)
+	fold_size = int(len(dataset) / n_folds)
+	for i in range(n_folds):
+		fold = list()
+		while len(fold) < fold_size:
+			index = randrange(len(dataset_copy))
+			fold.append(dataset_copy.pop(index))
+		dataset_split.append(fold)
+	return dataset_split
+
+# Calculate accuracy percentage
+def accuracy_metric(actual, predicted):
+	correct = 0
+	for i in range(len(actual)):
+		if actual[i] == predicted[i]:
+			correct += 1
+	return correct / float(len(actual)) * 100.0
+
+# Evaluate an algorithm using a cross validation split
+def evaluate_algorithm(dataset, algorithm, n_folds, *args):
+	folds = cross_validation_split(dataset, n_folds)
+	scores = list()
+	for fold in folds:
+		train_set = list(folds)
+		train_set.remove(fold)
+		train_set = sum(train_set, [])
+		test_set = list()
+		for row in fold:
+			row_copy = list(row)
+			test_set.append(row_copy)
+			row_copy[-1] = None
+		predicted = algorithm(train_set, test_set, *args)
+		actual = [row[-1] for row in fold]
+		accuracy = accuracy_metric(actual, predicted)
+		scores.append(accuracy)
+	return scores
+
+# Split a dataset based on an attribute and an attribute value
+def test_split(index, value, dataset):
+	left, right = list(), list()
+	for row in dataset:
+		if row[index] < value:
+			left.append(row)
+		else:
+			right.append(row)
+	return left, right
+
+# Calculate the Gini index for a split dataset
+def gini_index(groups, class_values):
+	gini = 0.0
+	for class_value in class_values:
+		for group in groups:
+			size = len(group)
+			if size == 0:
+				continue
+			proportion = [row[-1] for row in group].count(class_value) / float(size)
+			gini += (proportion * (1.0 - proportion))
+	return gini
+
+# Select the best split point for a dataset
+def get_split(dataset):
+	class_values = list(set(row[-1] for row in dataset))
+	b_index, b_value, b_score, b_groups = 999, 999, 999, None
+	for index in range(len(dataset[0])-1):
+		for row in dataset:
+			groups = test_split(index, row[index], dataset)
+			gini = gini_index(groups, class_values)
+			if gini < b_score:
+				b_index, b_value, b_score, b_groups = index, row[index], gini, groups
+	return {'index':b_index, 'value':b_value, 'groups':b_groups}
+
+# Create a terminal node value
+def to_terminal(group):
+	outcomes = [row[-1] for row in group]
+	return max(set(outcomes), key=outcomes.count)
+
+# Create child splits for a node or make terminal
+def split(node, max_depth, min_size, depth):
+	left, right = node['groups']
+	del(node['groups'])
+	# check for a no split
+	if not left or not right:
+		node['left'] = node['right'] = to_terminal(left + right)
+		return
+	# check for max depth
+	if depth >= max_depth:
+		node['left'], node['right'] = to_terminal(left), to_terminal(right)
+		return
+	# process left child
+	if len(left) <= min_size:
+		node['left'] = to_terminal(left)
+	else:
+		node['left'] = get_split(left)
+		split(node['left'], max_depth, min_size, depth+1)
+	# process right child
+	if len(right) <= min_size:
+		node['right'] = to_terminal(right)
+	else:
+		node['right'] = get_split(right)
+		split(node['right'], max_depth, min_size, depth+1)
+
+# Build a decision tree
+def build_tree(train, max_depth, min_size):
+	root = get_split(train)
+	split(root, max_depth, min_size, 1)
+	return root
+
+# Make a prediction with a decision tree
+def predict(node, row):
+	if row[node['index']] < node['value']:
+		if isinstance(node['left'], dict):
+			return predict(node['left'], row)
+		else:
+			return node['left']
+	else:
+		if isinstance(node['right'], dict):
+			return predict(node['right'], row)
+		else:
+			return node['right']
+
+# Classification and Regression Tree Algorithm
+def decision_tree(train, test, max_depth, min_size):
+	tree = build_tree(train, max_depth, min_size)
+
+	predictions = list()
+	for row in test:
+		prediction = predict(tree, row)
+		predictions.append(prediction)
+	return(predictions)
+
+def print_tree(node, depth=0):
+	if isinstance(node, dict):
+		print('%s[X%d < %.3f]' % ((depth*' ', (node['index']+1), node['value'])))
+		print_tree(node['left'], depth+1)
+		print_tree(node['right'], depth+1)
+	else:
+		print('%s[%s]' % ((depth*' ', node)))
+
+
+# load and prepare data
+filenameTrain = 'knn_train2.csv'
+filenameTest = 'knn_test2.csv'
+datasetTrain = load_csv(filenameTrain)
+datasetTest = load_csv(filenameTest)
+# convert string attributes to integers
+for i in range(len(datasetTrain[0])):
+	str_column_to_float(datasetTrain, i)
+for i in range(len(datasetTest[0])):
+	str_column_to_float(datasetTest, i)
+# evaluate algorithm
+n_folds = 5
+max_depth = 6
+min_size = 10
+scoresTrain = evaluate_algorithm(datasetTrain, decision_tree, n_folds, max_depth, min_size)
+print('Printing Accuracy for training data...')
+print('Scores: %s' % scoresTrain)
+print('Mean Accuracy: %.3f%%' % (sum(scoresTrain)/float(len(scoresTrain))))
+
+scoresTest = evaluate_algorithm(datasetTest, decision_tree, n_folds, max_depth, min_size)
+print('Printing Accuracy for testing data...')
+print('Scores: %s' % scoresTest)
+print('Mean Accuracy: %.3f%%' % (sum(scoresTest)/float(len(scoresTest))))
+
